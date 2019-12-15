b4792b567071a14920318d2810436541a721ca85
louismeeckers <louis@meeckers.be>
2019-12-15 23:32:31 +0100

    Add characteristics and google map to studentroom

--- app/src/main/java/com/example/kotshare/view/activities/StudentRoomActivity.java
+++ app/src/main/java/com/example/kotshare/view/activities/StudentRoomActivity.java
@@ -1,6 +1,7 @@
 package com.example.kotshare.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,6 +10,12 @@ import com.example.kotshare.R;
 import com.example.kotshare.data_access.StudentRoomDAO;
 import com.example.kotshare.data_access.StudentRoomDataAccess;
 import com.example.kotshare.model.StudentRoom;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
@@ -16,7 +23,7 @@ import com.smarteist.autoimageslider.SliderView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class StudentRoomActivity extends AppCompatActivity {
+public class StudentRoomActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @BindView(R.id.textView_singleStudentRoomTitle)
     TextView textView_singleStudentRoomTitle;
@@ -24,6 +31,9 @@ public class StudentRoomActivity extends AppCompatActivity {
     SliderView sliderStudentRoom;
     private StudentRoom studentRoom;
     private StudentRoomDataAccess studentRoomDataAccess;
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
 
     public StudentRoomActivity()
     {
@@ -52,6 +62,20 @@ public class StudentRoomActivity extends AppCompatActivity {
                 sliderStudentRoom.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
             }
         }
+
+        // Google map
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mapAPI = googleMap;
+        LatLng aubel = new LatLng(50.6970841, 5.8245657);
+        mapAPI.addMarker(new MarkerOptions().position(aubel).title("Aubel"));
+        float zoom = (float)11.0;
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(aubel, zoom));
     }
 
 }
